09fb00357e52a222ca40b2149e42bff495c65562
JulesSacha <fernsner.j@gmail.com>
2021-02-17 12:43:53 +0100

    commit_name

---
+++ src/Controller/ContestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContestController extends AbstractController
+{
+    /**
+     * @Route("/contest", name="contest")
+     */
+    public function index(): Response
+    {
+        return $this->render('contest/index.html.twig', [
+            'controller_name' => 'ContestController',
+        ]);
+    }
+}
